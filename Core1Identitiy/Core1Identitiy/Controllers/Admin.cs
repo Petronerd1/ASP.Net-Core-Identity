cbeaca90ceca00b3cf5affbf866415ed6dd643bd
Petronerd1 <bedirhanttepe@gmail.com>
2021-07-28 12:22:42 +0300

    Identity Kullanımı
    Telepati Yazılım (Eğitim)

---
+++ Core1Identitiy/Core1Identitiy/Controllers/Admin.cs
@@ -0,0 +1,51 @@
+﻿using Core1Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Core1Identity.Controllers
+{
+    public class Admin : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        public Admin(UserManager<ApplicationUser> _userManager)
+        {
+            userManager = _userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+
+                var result = await userManager.CreateAsync(user,model.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                        {
+                        ModelState.AddModelError("", item.Description);
+
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
